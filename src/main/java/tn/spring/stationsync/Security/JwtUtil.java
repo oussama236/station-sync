ca576265acb05a00d3dc6ca21e5631c9e99251ec
Oussama.Miladi <oussama.miladi@esprit.tn>
2025-11-29 02:58:20 +0100

    token expiry

--- src/main/java/tn/spring/stationsync/Security/JwtUtil.java
+++ src/main/java/tn/spring/stationsync/Security/JwtUtil.java
@@ -26,7 +26,7 @@ public class JwtUtil {
 
     private SecretKey SECRET_KEY;
 
-    private static final long EXPIRATION_TIME = 1000L * 60 * 60; // 1 hour
+    private static final long EXPIRATION_TIME = 1000L * 60 * 60 * 24; // 24 hours
     private static final long CLOCK_SKEW_SECONDS = 60L;
 
     @PostConstruct
